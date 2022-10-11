e270cd28e8980485ae5a27c07b5ce9d08528bf03
jemiller <jmiller@JackBook.local>
2022-10-10 23:03:50 -0400

    a4 bootstrap fall 2022

---
+++ public/labs/a5/index.js
@@ -0,0 +1,222 @@
+// alert('Hello World!');
+
+console.log('Hello World!');
+
+console.log('Variables and Constants');
+// eslint-disable-next-line no-undef
+global1 = 10;
+var functionScoped = 2;
+let blockScoped = 5;
+// eslint-disable-next-line no-undef
+const constant1 = global1
+    + functionScoped
+    - blockScoped;
+
+console.log(global1)
+console.log(functionScoped)
+console.log(blockScoped)
+console.log(constant1)
+
+function printVars(variables) {
+    for (let i=0; i<variables.length; i++) {
+        const string1 = variables[i];
+        console.log(string1);
+    }
+}
+
+console.log('Variable types');
+let numberVariable = 123;
+let floatingPointNumber = 234.345;
+let stringVariable = 'Hello World!';
+let booleanVariable = true;
+let isNumber = typeof numberVariable;
+let isString = typeof stringVariable;
+let isBoolean = typeof booleanVariable;
+const variables = [numberVariable,
+    floatingPointNumber,
+    stringVariable,
+    booleanVariable,
+    isNumber,
+    isString,
+    isBoolean
+]
+printVars(variables)
+
+console.log('Boolean Variables');
+let true1 = true;
+let false1 = false;
+let false2 = true1 && false1;
+let true2 = true1 || false1;
+let true3 = !false2;
+let true4 = numberVariable === 123;
+let true5 = floatingPointNumber !== 321.432;
+let false3 = numberVariable < 100;
+let sortaTrue = '1' == 1
+let notTrue   = '1' === 1
+
+let variables2 = [
+    true1,
+    false1,
+    false2,
+    true2,
+    true3,
+    true4,
+    true5,
+    false3,
+    sortaTrue,
+    notTrue
+]
+printVars(variables2);
+
+console.log('If else');
+if(true1) {
+    console.log(true);
+}
+
+if(!false1) {
+    console.log('!false1');
+} else {
+    console.log('false1');
+}
+
+console.log('Ternary conditional operator');
+const loggedIn = true;
+const greeting = loggedIn ? 'Welcome!' : 'Please login';
+console.log(greeting)
+
+console.log('Legacy ES5 function')
+function add (a, b) {
+    return a + b;
+}
+const twoPlusFour = add(2, 4);
+console.log(twoPlusFour);
+
+console.log('New ES6 functions')
+const subtract = (a, b) => {
+    return a - b;
+}
+const threeMinusOne = subtract(3, 1);
+console.log(threeMinusOne);
+
+console.log('Implied Return')
+const multiply = (a, b) => a * b;
+const fourTimesFive = multiply(4, 5);
+console.log(fourTimesFive);
+
+console.log('Parenthesis and Parameters')
+const square = a => a * a;
+const plusOne = a => a + 1;
+const twoSquared = square(2);
+const threePlusOne = plusOne(3);
+console.log(twoSquared);
+console.log(threePlusOne);
+
+console.log('Arrays')
+let numberArray1 = [1, 2, 3, 4, 5];
+let stringArray1 = ['string1', 'string2'];
+let variableArray1 = [
+    functionScoped,
+    blockScoped,
+    constant1,
+    numberArray1,
+    stringArray1
+];
+const vars3 = [
+    numberArray1, stringArray1, variableArray1
+]
+printVars(vars3);
+
+console.log('Array index and length')
+const length1 = numberArray1.length;
+const index1 = numberArray1.indexOf(3);
+console.log(length1);
+console.log(index1);
+
+
+console.log('Add and remove data to arrays')
+// adding new items
+numberArray1.push(6);
+stringArray1.push('string3');
+
+// remove 1 item starting on 3rd spot
+numberArray1.splice(2, 1);
+stringArray1.splice(1, 1);
+console.log(numberArray1);
+console.log(stringArray1);
+
+
+console.log('For loops');
+for (let i=0; i<stringArray1.length; i++) {
+    const string1 = stringArray1[i];
+    console.log(string1);
+}
+
+console.log('Map function');
+const squares = numberArray1.map(square);
+const cubes = numberArray1.map(a => a * a * a);
+console.log(squares);
+console.log(cubes);
+
+console.log('Find function');
+const four = numberArray1.find(a => a === 4);
+const string3 = stringArray1.find(a => a === 'string3');
+console.log(four);
+console.log(string3);
+
+console.log('Find index')
+const fourIndex = numberArray1
+    .findIndex(a => a === 4);
+const string3Index = stringArray1
+    .findIndex(a => a === 'string3');
+console.log(fourIndex)
+console.log(string3Index)
+
+
+console.log('Filter function')
+const numbersGreaterThan2 = numberArray1
+    .filter(a => a > 2);
+const evenNumbers = numberArray1
+    .filter(a => a % 2 === 0);
+const oddNumbers = numberArray1
+    .filter(a => a % 2 !== 0);
+console.log(numbersGreaterThan2)
+console.log(evenNumbers)
+console.log(oddNumbers)
+
+
+console.log('Template strings')
+const five = 2 + 3;
+const result1 = "2 + 3 = " + five;
+console.log(result1);
+
+const result2 = `2 + 3 = ${2 + 3}`;
+console.log(result2);
+
+const username = "alice";
+const greeting1 = `Welcome home ${username}`;
+console.log(greeting1);
+
+const loggedIn2 = false;
+const greeting2 = `Logged in: ${loggedIn2 ? "Yes" : "No"}`;
+console.log(greeting2);
+
+const init = () => {
+    console.log('Hello world from jQuery');
+    /* do the rest of the lab work here */
+    const bindById = $('#bind-by-id');
+    const bindByClass = $('.bind-by-class');
+    console.log('Binding to DOM');
+    console.log(bindById);
+    console.log(bindByClass);
+
+    const changeStyle = $('#change-style');
+    const changeStyle2 = $('.change-style');
+    changeStyle.css('color', 'red');
+    changeStyle2.css('color', 'blue');
+
+    const getIdAttr = $("#get-id-attr");
+    const id = getIdAttr.attr('id');
+    console.log(id);
+}
+$(init);
+
